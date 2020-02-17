8ae2683c82901662208f97687145d44305048f1e
Lucas Alves Dos Santos <lucas201273@gmail.com>
2020-02-16 21:04:25 -0300

    Conectando a Database de forma local

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express= require('express')
+const mongoose= require('mongoose')
 
+//Iniciando o App
 const app= express()
 
+//Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/Nodeapi',
+{useNewUrlParser:true,
+useUnifiedTopology:true})
 
+
+//Primeira Rota
 app.get('/', (request, response)=>{
 
-    response.send('Hello Rocketseat')
+    response.send('Mudei para a  Rocketseat')
 
 })
 
